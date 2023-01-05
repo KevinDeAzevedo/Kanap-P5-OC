1bc47ebec6e102c4d8fe0f917a1b978e301f4587
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-01-05 16:54:44 +0100

    fix color

--- front/src/choosenproducts.js
+++ front/src/choosenproducts.js
@@ -32,7 +32,9 @@ function makeHtmlCartList(param) {
       product.imageUrl +
       '" alt=""></div><div class="cart__item__content"><div class="cart__item__content__description"><h2>' +
       product.name +
-      '</h2><p>Vert</p><p>' +
+      '</h2><p>' +
+      product.color +
+      '</p><p>' +
       product.price +
       ' €</p></div><div class="cart__item__content__settings"><div class="cart__item__content__settings__quantity"><p>Qté : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="' +
       product.quantity +
